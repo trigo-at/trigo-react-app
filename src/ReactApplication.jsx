d70129f8585db1447943c9bb35f1e7570ef3c40e
David Wippel <david@trigo.at>
2017-04-10 13:33:04 +0200

    chore: migrate to prop-types

--- src/ReactApplication.jsx
+++ src/ReactApplication.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes, Component } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router5';
 import { ThemeProvider, injectGlobal } from 'styled-components';
